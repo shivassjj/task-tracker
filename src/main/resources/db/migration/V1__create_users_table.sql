5a0954abe521071c660ef7d01f8f0f3767607ffb
Dima Panchenko <panchenkod2002@mail.ru>
2025-12-02 19:50:46 +0300

    feat: added migrations for users and projects

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__create_projects_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE projects (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT
+);
